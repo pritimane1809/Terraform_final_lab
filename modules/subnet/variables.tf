deefb746470f32b5793db33848f4d6f0afb8f594
Priti Mane <pritimane1809@gmail.com>
2026-05-15 14:56:16 +0530

    third commit

--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -1,5 +1,24 @@
-variable "subnet_name" {}
-variable "vpc_id" {}
-variable "cidr" {}
-variable "availability_zone" {}
-variable "tags" {}
+variable "subnet_name" {
+    type = string
+    description = "The name of the subnet"
+}
+
+variable "vpc_id" {
+    type = string
+    description = "The ID of the VPC"
+}
+
+variable "cidr" {
+    type = string
+    description = "The CIDR block for the subnet"
+}
+
+variable "availability_zone" {
+    type = string
+    description = "The availability zone for the subnet"
+}
+
+variable "tags" {
+    type = map(string)
+    description = "Tags to apply to the subnet"
+}
